74234a2028bbfd248769bc05fad18cd9d2a231cc
cheriels <cschultz@icprb.org>
2017-03-16 15:33:24 -0400

    first commit

---
+++ Hello2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 13:49:39 2017
+
+@author: cschultz
+"""
+import pylab
+def hello():
+    print("Hello again")
+hello()
+pylab.plot(range(10),'o')
